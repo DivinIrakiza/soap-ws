46b0ebf8174e4b6eeb0fe010a1b623079ea8e2c2
Divin Irakiza <irakizadivin@gmail.com>
2021-06-21 21:37:10 +0200

    First commit

---
+++ src/test/java/com/rca/studies/spring/soapws/SoapWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rca.studies.spring.soapws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
