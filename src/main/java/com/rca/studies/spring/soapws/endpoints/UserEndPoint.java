b0315ee518c6a103502cf339fcf5c97333bdd095
Divin Irakiza <irakizadivin@gmail.com>
2021-06-21 22:45:18 +0200

    feat: UserEndPoint implementation done

--- src/main/java/com/rca/studies/spring/soapws/endpoints/UserEndPoint.java
+++ src/main/java/com/rca/studies/spring/soapws/endpoints/UserEndPoint.java
@@ -1,7 +1,25 @@
 package com.rca.studies.spring.soapws.endpoints;
 
+import com.rca.studies.spring.soapws.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import studies.rca.com.soap.spring.soapws.GetUserRequest;
+import studies.rca.com.soap.spring.soapws.GetUserResponse;
 
 @Endpoint
 public class UserEndPoint {
+    @Autowired
+    private UserService userService;
+
+
+    @PayloadRoot(namespace = "http://soap.com.rca.studies/spring/soapws", localPart = "getUserRequest")
+    @ResponsePayload
+    public GetUserResponse getUserRequest(@RequestPayload GetUserRequest request) {
+        GetUserResponse response = new GetUserResponse();
+        response.setUser(userService.getUsers(request.getName()));
+        return response;
+    }
 }
