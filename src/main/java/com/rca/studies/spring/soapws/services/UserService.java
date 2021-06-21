befe86420142308164f777a1ae28b1769a31350b
Divin Irakiza <irakizadivin@gmail.com>
2021-06-21 22:28:50 +0200

    new: UserService initiation

--- src/main/java/com/rca/studies/spring/soapws/services/UserService.java
+++ src/main/java/com/rca/studies/spring/soapws/services/UserService.java
@@ -1,8 +1,10 @@
 package com.rca.studies.spring.soapws.services;
 
 import org.springframework.stereotype.Service;
-
+import studies.rca.com.soap.spring.soapws.User;
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -13,22 +15,25 @@ public class UserService {
     public void initialize() {
 
         User user1 = new User();
-        user1.setName("Peter");
-        user1.setEmpId(1111);
+        user1.setId(1);
+        user1.setName("User 1");
         user1.setSalary(12000);
 
         User user2 = new User();
-        user2.setName("Sam");
-        user2.setEmpId(1112);
+        user2.setId(2);
+        user2.setName("User 2");
         user2.setSalary(32000);
 
         User user3 = new User();
-        user3.setName("Ryan");
-        user3.setEmpId(1113);
+        user3.setId(3);
+        user3.setName("User 3");
         user3.setSalary(16000);
 
         users.put(user1.getName(), user1);
         users.put(user2.getName(), user2);
         users.put(user3.getName(), user3);
+    }
+    public User getUsers(String name) {
+        return users.get(name);
     }
 }
