24f3b4d2a5aded5296c529ece81350622ff9fba7
Divin Irakiza <irakizadivin@gmail.com>
2021-06-25 15:42:42 +0200

    (new): end-point getSUpplierById done

--- src/main/java/com/rca/spring/exam/soapws/endpoints/SupplierEndPoint.java
+++ src/main/java/com/rca/spring/exam/soapws/endpoints/SupplierEndPoint.java
@@ -31,12 +31,7 @@ public class SupplierEndPoint {
         GetAllSuppliersResponse response = new GetAllSuppliersResponse();
 
         for (SupplierModel supplier: suppliers){
-            Supplier _supplier = new Supplier();
-            _supplier.setId(supplier.getId());
-            _supplier.setEmail(supplier.getEmail());
-            _supplier.setNames(supplier.getNames());
-            _supplier.setMobile(supplier.getMobile());
-
+            Supplier _supplier = mapSupplier(supplier);
             response.getSupplier().add(_supplier);
         }
 
@@ -54,11 +49,7 @@ public class SupplierEndPoint {
 
         GetSupplierResponse response = new GetSupplierResponse();
 
-        Supplier _supplier = new Supplier();
-        _supplier.setId(supplier.get().getId());
-        _supplier.setEmail(supplier.get().getEmail());
-        _supplier.setNames(supplier.get().getNames());
-        _supplier.setMobile(supplier.get().getMobile());
+        Supplier _supplier = mapSupplier(supplier.get());
 
         response.setSupplier(_supplier);
 
@@ -66,35 +57,37 @@ public class SupplierEndPoint {
     }
 
 
-    @PayloadRoot(namespace = "com.rca.spring.exam/divinirakiza/soapws", localPart = "NewStudentDTORequest")
+    @PayloadRoot(namespace = "com.rca.spring.exam/divinirakiza/soapws", localPart = "CreateSupplierRequest")
     @ResponsePayload
-    public NewStudentDTOResponse create(@RequestPayload NewStudentDTORequest dto) {
-        https.rca_ac_rw.verie.soap_app.student.Student __student = dto.getStudent();
-        Student _student = new Student(__student.getFirstName(), __student.getFirstName(), __student.getGender());
-        Student student = studentRepository.save(_student);
-        NewStudentDTOResponse studentDTO = new NewStudentDTOResponse();
-        __student.setId(student.getId());
-        studentDTO.setStudent(__student);
-        return studentDTO;
-    }
+    public CreateSupplierResponse create(@RequestPayload CreateSupplierRequest dto) {
+        SupplierDTO supplierDTO = dto.getSupplier();
 
+        SupplierModel supplier = new SupplierModel();
+        supplier.setEmail(supplierDTO.getEmail());
+        supplier.setNames(supplierDTO.getNames());
+        supplier.setMobile(supplierDTO.getMobile());
 
-    @PayloadRoot(namespace = "com.rca.spring.exam/divinirakiza/soapws", localPart = "DeleteStudentRequest")
-    @ResponsePayload
-    public DeleteStudentResponse delete(@RequestPayload DeleteStudentRequest request){
-        studentRepository.deleteById(request.getId());
-        DeleteStudentResponse response = new DeleteStudentResponse();
-        response.setMessage("Successfully deleted a message");
+        SupplierModel entity = this.supplierRepository.save(supplier);
+        CreateSupplierResponse response = new CreateSupplierResponse();
+
+        response.setSupplier(mapSupplier(entity));
+        response.setSuccess(true);
         return response;
     }
 
-    @PayloadRoot(namespace = "com.rca.spring.exam/divinirakiza/soapws", localPart = "UpdateStudentRequest")
-    @ResponsePayload
-    public UpdateStudentResponse update(@RequestPayload UpdateStudentRequest request){
-        https.rca_ac_rw.verie.soap_app.student.Student __student = request.getStudent();
 
-        Student _student = new Student(__student.getFirstName(), __student.getFirstName(), __student.getGender());
-        _student.setId(__student.getId());
+    @PayloadRoot(namespace = "com.rca.spring.exam/divinirakiza/soapws", localPart = "UpdateSupplierRequest")
+    @ResponsePayload
+    public UpdateSupplierResponse update(@RequestPayload UpdateSupplierRequest request){
+        SupplierDTO supplierDTO = request.getSupplier();
+
+        Optional<SupplierModel> supplier = this.supplierRepository.findById());
+
+
+        SupplierModel supplier = new SupplierModel();
+        supplier.setEmail(supplierDTO.getEmail());
+        supplier.setNames(supplierDTO.getNames());
+        supplier.setMobile(supplierDTO.getMobile());
 
         Student student = studentRepository.save(_student);
 
@@ -105,5 +98,26 @@ public class SupplierEndPoint {
         studentDTO.setStudent(__student);
 
         return studentDTO;
+    }
+
+    @PayloadRoot(namespace = "com.rca.spring.exam/divinirakiza/soapws", localPart = "DeleteStudentRequest")
+    @ResponsePayload
+    public DeleteStudentResponse delete(@RequestPayload DeleteStudentRequest request){
+        studentRepository.deleteById(request.getId());
+        DeleteStudentResponse response = new DeleteStudentResponse();
+        response.setMessage("Successfully deleted a message");
+        return response;
+    }
+
+
+
+    private Supplier mapSupplier(SupplierModel supplierModel) {
+        Supplier supplier = new Supplier();
+        supplier.setId(supplierModel.getId());
+        supplier.setEmail(supplierModel.getEmail());
+        supplier.setNames(supplierModel.getNames());
+        supplier.setMobile(supplierModel.getMobile());
+
+        return supplier;
     }
 }}
